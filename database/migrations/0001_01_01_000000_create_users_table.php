c63de56b0ac9e90e7b3b611641564a4b7f5c13fb
YahirSib <yahirstewart16@gmail.com>
2025-04-16 14:37:44 -0600

    Creation of migrate PRODUCTS

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -11,6 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+        Schema::create('perfil', function (Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->string('estado');
+        });
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
@@ -19,6 +26,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreignId('perfil_id')->constrained('perfil', 'id'); 
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
@@ -35,6 +43,7 @@ return new class extends Migration
             $table->longText('payload');
             $table->integer('last_activity')->index();
         });
+ 
     }
 
     /**
@@ -45,5 +54,6 @@ return new class extends Migration
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
+        Schema::dripIfExists('perfil');
     }
 };
